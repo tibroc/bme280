0a4b8441604b9f16e62c8426ad9dbd0588db8df3
tibroc <timo.brockmeyer@gmail.com>
2019-04-16 23:34:40 +0200

    further condensed the set/get code and added missing functions

--- bme280.py
+++ bme280.py
@@ -42,7 +42,7 @@ class bme280_instance:
         chip = self.i2c.readfrom_mem(constants.BME_I2C_ADDR, constants.REG_ID, 2)
         chip_id = unpack("<h", chip)[0]
         if chip_id != 0x60:
-            raise OSError("The i2c device does not return the correct chip id.")
+            raise OSError("The i2c device does not return the correct chip id: {}".format(chip_id))
 
     def reset_sensor(self):
         self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_RESET, bytearray([0xB6]))
@@ -58,137 +58,133 @@ class bme280_instance:
         h5 = dig26_41[3] // 16
         self._calibration_h = (dig00_25[13], dig26_41[0], dig26_41[1], dig26_41[2], h4, h5)
 
-    def set_temp_oversampling(self, rate):
-        if rate not in ['x1', 'x2', 'x4', 'x8', 'x16']:
-            raise TypeError("'rate' has to be one of ['x1', 'x2', 'x4', 'x8', 'x16']")
+    def _set_value(self, value, register, bit_mask):
+        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, register, 1)
+        ctrl_byte = unpack('<b', ctrl_byte)[0]
+        out_value = (ctrl_byte & bit_mask) | value
+        self.i2c.writeto_mem(self.bme_i2c_addr, register, bytearray([out_value]))
+    
+    def _get_value(self, register, bit_mask):
         ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
         ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0x1F
-        if rate == 'x1':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_T_1
-        if rate == 'x2':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_T_2
-        if rate == 'x4':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_T_4
-        if rate == 'x8':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_T_8
-        if rate == 'x16':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_T_16
-        self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, bytearray([ctrl_byte]))
+        bit_mask = 0x03
+        return ctrl_byte & bit_mask
+
+    def set_temp_oversampling(self, rate):
+        self._set_value(rate, constants.REG_CTRL_MEAS, 0x1F)
 
     def get_temp_oversampling(self):
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0xE0
-        value = ctrl_byte & bit_mask
+        value = self._get_value(constants.REG_CTRL_MEAS, 0xE0)
         if value == constants.OS_T_1:
-            return 'x1'
+            return 'x1', value
         if value == constants.OS_T_2:
-            return 'x2'
+            return 'x2', value
         if value == constants.OS_T_4:
-            return 'x4'
+            return 'x4', value
         if value == constants.OS_T_8:
-            return 'x8'
+            return 'x8', value
         if value in [constants.OS_T_16, 0xE0, 0xC0]:
-            return 'x16'
-        raise RuntimeError("Something went wrong. Did you configure the sensor?")
+            return 'x16', value
+        else
+            return 'reset state', value
         
     def set_press_oversampling(self, rate):
-        if rate not in ['x1', 'x2', 'x4', 'x8', 'x16']:
-            raise TypeError("'rate' has to be one of ['x1', 'x2', 'x4', 'x8', 'x16']")
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0xE3
-        if rate == 'x1':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_P_1
-        if rate == 'x2':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_P_2
-        if rate == 'x4':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_P_4
-        if rate == 'x8':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_P_8
-        if rate == 'x16':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_P_16
-        self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, bytearray([ctrl_byte]))
+        self._set_value(rate, constants.REG_CTRL_MEAS, 0xE3)
 
     def get_press_oversampling(self):
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0x1C
-        value = ctrl_byte & bit_mask
+        value = self._get_value(constants.REG_CTRL_MEAS, 0x1C)
         if value == constants.OS_P_1:
-            return 'x1'
+            return 'x1', value
         if value == constants.OS_P_2:
-            return 'x2'
+            return 'x2', value
         if value == constants.OS_P_4:
-            return 'x4'
+            return 'x4', value
         if value == constants.OS_P_8:
-            return 'x8'
+            return 'x8', value
         if value in [constants.OS_P_16, 0x1C, 0x18]:
-            return 'x16'
-        raise RuntimeError("Something went wrong. Did you configure the sensor?")
+            return 'x16', value
+        else
+            return 'reset state', value
 
     def set_hum_oversampling(self, rate):
-        if rate not in ['x1', 'x2', 'x4', 'x8', 'x16']:
-            raise TypeError("'rate' has to be one of ['x1', 'x2', 'x4', 'x8', 'x16']")
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_HUM, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0xF8
-        if rate == 'x1':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_H_1
-        if rate == 'x2':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_H_2
-        if rate == 'x4':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_H_4
-        if rate == 'x8':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_H_8
-        if rate == 'x16':
-            ctrl_byte = (ctrl_byte & bit_mask) | constants.OS_H_16
-        self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_CTRL_HUM, bytearray([ctrl_byte]))
-        # it is necessary to write to ctrl_meas for the changes in ctrl_hum to take place
+        self._set_value(rate, constants.REG_CTRL_HUM, 0xF8)
+        # it is necessary to do a write operation to ctrl_meas for the changes in ctrl_hum to take place
         ctrl_meas = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
         ctrl_meas = unpack('<b', ctrl_byte)[0]
         self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, bytearray([ctrl_meas]))
 
     def get_hum_oversampling(self):
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_HUM, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0x07
-        value = ctrl_byte & bit_mask
+        value = self._get_value(constants.REG_CTRL_HUM, 0x07)
         if value == constants.OS_H_1:
-            return 'x1'
+            return 'x1', value
         if value == constants.OS_H_2:
-            return 'x2'
+            return 'x2', value
         if value == constants.OS_H_4:
-            return 'x4'
+            return 'x4', value
         if value == constants.OS_H_8:
-            return 'x8'
+            return 'x8', value
         if value in [constants.OS_H_16, 0x07, 0x06]:
-            return 'x16'
-        raise RuntimeError("Something went wrong. Did you configure the sensor?")
+            return 'x16', value
+        else
+            return 'reset state', value
 
     def set_mode(self, mode):
-        if mode not in ['sleep', 'force', 'normal']:
-            raise TypeError("'mode' has to be one of ['sleep', 'force', 'normal']")
-        bit_mask = 0xFC
-        if mode == 'sleep':
-            self._ctrl_meas = (self._ctrl_meas & bit_mask) | constants.MODE_SLEEP
-        if mode == 'force':
-            self._ctrl_meas = (self._ctrl_meas & bit_mask) | constants.MODE_FORCE
-        if mode == 'normal':
-            self._ctrl_meas = (self._ctrl_meas & bit_mask) | constants.MODE_NORMAL
-        self.i2c.writeto_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, bytearray([self._ctrl_meas]))
+        self._set_value(mode, constants.REG_CTRL_MEAS, 0xFC)
 
     def get_mode(self):
-        ctrl_byte = self.i2c.readfrom_mem(self.bme_i2c_addr, constants.REG_CTRL_MEAS, 1)
-        ctrl_byte = unpack('<b', ctrl_byte)[0]
-        bit_mask = 0x03
-        value = ctrl_byte & bit_mask
+        value = self._get_value(constants.REG_CTRL_MEAS, 0x03)
         if value == constants.MODE_SLEEP:
-            return 'sleep'
-        if value == constants.MODE_FORCE:
-            return 'force'
+            return 'sleep', value
+        if value in [constants.MODE_FORCE, 0x02]:
+            return 'force', value
         if value == constants.MODE_NORMAL:
-            return 'normal'
-        raise RuntimeError("Something went wrong. Did you configure the sensor?")
+            return 'normal', value
+
+    def set_time_standby(self, interval):
+        self._set_value(interval, constants.REG_CONFIG, 0x1F)
+
+    def get_time_standby(self):
+        value = self._get_value(constants.REG_CONFIG, 0xE0)
+        if value == constants.TSB_0_5:
+            return '0.5ms', value
+        if value == constants.TSB_62_5:
+            return '62.5ms', value
+        if value == constants.TSB_125:
+            return '125ms', value
+        if value == constants.TSB_250:
+            return '250ms', value
+        if value == constants.TSB_500:
+            return '500ms', value
+        if value == constants.TSB_1000:
+            return '1000ms', value
+        if value == constants.TSB_10:
+            return '10ms', value
+        if value == constants.TSB_20:
+            return '20ms', value
+
+    def set_filter(self, mode):
+        self._set_value(mode, constants.REG_CONFIG, 0xE3)
+
+    def get_filter(self):
+        value = self._get_value(constants.REG_CONFIG, 0x1C)
+        if value == constants.FILTER_OFF:
+            return 'off', value
+        if value == constants.FILTER_2:
+            return '2x', value
+        if value == constants.FILTER_4:
+            return '4x', value
+        if value == constants.FILTER_8:
+            return '8x', value
+        if value in [constants.FILTER_16, 0x14, 0x18, 0x1C]:
+            return '16x', value
+
+    def set_spi3w(self, mode):
+        self._set_value(mode, constants.REG_CONFIG, 0xFE)
+
+    def get_spi3w(self):
+        value = self._get_value(constants.REG_CONFIG, 0x01)
+        if value == constants.SPI3W_ON:
+            return 'on', value
+        if value == constants.SPI3W_OFF:
+            return 'off', value
 
